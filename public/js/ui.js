48cdfb6304f2f7adecb918bbf067a12e53f0acba
Johannes Gunnback <johannes.gunnback@aptitud.se>
2014-10-24 12:44:48 +0200

    Ny prickhantering

--- public/js/ui.js
+++ public/js/ui.js
@@ -1,3 +1,4 @@
+/*
 function init(h,w,padding) {
     $('#title').text(document.title);
 
@@ -6,11 +7,66 @@ function init(h,w,padding) {
         .height(h)
         .canvas('radar')
 
-    var radar_arcs = [{r:100,name:"adopt",bgcolor:"#fafafa"}
-        ,{r:200,name:"trial",bgcolor:"#ccc"}
-        ,{r:300,name:"assess",bgcolor:"#aaa"}
-        ,{r:400,name:"hold",bgcolor:"#999"}].reverse();
+    var radar_arcs = [
+        {r: 100, name: "adopt", bgcolor: "#fafafa"}
+        ,
+        {r: 200, name: "trial", bgcolor: "#ccc"}
+        ,
+        {r: 300, name: "assess", bgcolor: "#aaa"}
+        ,
+        {r: 400, name: "hold", bgcolor: "#999"}
+    ].reverse();
 
+    var radar_data = [
+        { "quadrant": "Techniques",
+            "left": 45,
+            "top": 18,
+            "color": "#8FA227",
+            "items": [
+                {"name": "Database based Integration", "pc": {"r": 350, "t": 135}, "movement": "t", "blipSize": 700},
+                {"name": "Scrum certification", "pc": {"r": 350, "t": 95}, "movement": "c", "url": "http://www.google.com"},
+                {"name": "Incremental data warehousing", "pc": {"r": 250, "t": 165}, "movement": "c"},
+                {"name": "DevOps", "pc": {"r": 250, "t": 110}, "movement": "c"},
+                {"name": "Polygot Programming", "pc": {"r": 180, "t": 170}, "movement": "c"},
+                {"name": "Automation of technical tests", "pc": {"r": 180, "t": 155}, "movement": "c"},
+                {"name": "Capability modelling", "pc": {"r": 180, "t": 125}, "movement": "c"},
+                {"name": "Service choreography", "pc": {"r": 180, "t": 105}, "movement": "c"},
+                {"name": "Continuous deployment", "pc": {"r": 180, "t": 100}, "movement": "c"},
+                {"name": "Evolutionary architecture", "pc": {"r": 120, "t": 95}, "movement": "c"},
+                {"name": "Coding architects", "pc": {"r": 90, "t": 170}, "movement": "c"},
+                {"name": "Visualisation and metrics", "pc": {"r": 80, "t": 150}, "movement": "c"},
+                {"name": "Web as platform", "pc": {"r": 80, "t": 110}, "movement": "c"},
+                {"name": "Emergent design", "pc": {"r": 80, "t": 100}, "movement": "c"},
+                {"name": "Evolutionary database", "pc": {"r": 70, "t": 170}, "movement": "c"},
+                {"name": "Platform roadmaps", "pc": {"r": 30, "t": 100}, "movement": "c"},
+                {"name": "Build pipelines", "pc": {"r": 30, "t": 160}, "movement": "c"}
+            ]
+        }
+    ];
+
+    function polar_to_cartesian(r,t) {
+        //radians to degrees, requires the t*pi/180
+        var x = r * Math.cos((t*Math.PI/180));
+        var y = r * Math.sin((t*Math.PI/180));
+        return [x,y];
+    }
+
+    function cartesian_to_raster(x,y) {
+        var rx = w/2 + x;
+        var ry = h/2 + y;
+        return [rx,ry];
+    }
+
+    function raster_to_cartesian(rx,ry) {
+        var x = rx - w/2;
+        var y = ry - h/2;
+        return [x,y];
+    }
+
+    function polar_to_raster(r,t) {
+        var xy= polar_to_cartesian(r,t);
+        return cartesian_to_raster(xy[0], xy[1]);
+    }
 
 // arcs
     radar.add(pv.Dot)
@@ -29,7 +85,7 @@ function init(h,w,padding) {
         .lineWidth(1)
         .left(w/2)
         .bottom(function(d) {return d;})
-        .strokeStyle("#000");
+        .strokeStyle("#ccc");
 
     //quadrant lines -- horizontal
     radar.add(pv.Line)
@@ -37,10 +93,10 @@ function init(h,w,padding) {
         .lineWidth(1)
         .bottom(h/2)
         .left(function(d) {return d;})
-        .strokeStyle("#000");
+        .strokeStyle("#ccc");
 
 
-    /*// blips
+// blips
     var total_index=1;
     for (var i = 0; i < radar_data.length; i++) {
         radar.add(pv.Dot)
@@ -64,11 +120,17 @@ function init(h,w,padding) {
             .add(pv.Label)
             .text(function(d) {return total_index++;})
             .textBaseline("middle")
-            .textStyle("white");
+            .textStyle("white")
+            .event('mousedown', function (d) {
+             console.log(d);
+            })
+            .event('drag', function(d){ console.log(d); });
     }
 
 
-//Quadrant Ledgends
+    */
+/*//*
+/Quadrant Ledgends
     var radar_quadrant_ctr=1;
     for (var i = 0; i < radar_data.length; i++) {
         radar.add(pv.Label)
@@ -90,9 +152,10 @@ function init(h,w,padding) {
             .anchor("right")
             .add(pv.Label)
             .text(function(d) {return radar_quadrant_ctr++ + ". " + d.name;} );
-    }*/
+    }*//*
+
 
     radar.anchor('radar');
     radar.render();
 
-};
+};*/
