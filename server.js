65fb82bda60139dca479dee1b28556cadd7a5292
Hugo Häggmark <hugo.haggmark@gmail.com>
2014-10-28 07:02:12 +0100

    added sockets

--- server.js
+++ server.js
@@ -1,33 +1,25 @@
-/**
- * Created by Zem on 2014-10-24.
- */
-/**
- * Created by Zem on 2014-10-21.
- */
-/**
- * Created by Zem on 2014-10-10.
- */
-// set up ========================
-var express  = require('express');
-var app      = express();
+var express = require('express'),
+  app = express(),
+  http = require('http').Server(app),
+  io = require('socket.io')(http);
+
 var bodyParser = require('body-parser');
 
-// configuration =================
-
-
-
 app.use(express.static(__dirname + '/public'));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.set('port', (process.env.PORT || 8080));
 
-// start app ======================================
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'))
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
 });
 
-
-
-app.get('*', function(req, res) {
-    res.sendfile(__dirname +'/public/index.html');
+http.listen(app.get('port'), function () {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
 
+app.get('/', function (req, res) {
+  res.sendFile('index.html');
+});
